38e8655bacb6b1c092ef815dd160273cc1b0e20a
bsalgautam16 <bishal.gautam@careem.com>
2020-12-18 14:29:02 +0545

    first-commit

---
+++ src/main/java/com/nepaliteacher/alexanepaliteacher/AlexaNepaliteacherApplication.java
@@ -0,0 +1,13 @@
+package com.nepaliteacher.alexanepaliteacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexaNepaliteacherApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AlexaNepaliteacherApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/nepaliteacher/alexanepaliteacher/AlexaNepaliteacherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nepaliteacher.alexanepaliteacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexaNepaliteacherApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
