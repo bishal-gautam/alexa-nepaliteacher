0ce12fc234c15fa9a7a0ab435091b7080f1adb56
bishal-gautam <bishal.gautam@careem.com>
2020-12-21 14:29:38 +0545

    handled fallback requests

--- src/main/java/com/nepaliteacher/alexanepaliteacher/handlers/FallBackIntentHandler.java
+++ src/main/java/com/nepaliteacher/alexanepaliteacher/handlers/FallBackIntentHandler.java
@@ -1,5 +1,7 @@
 package com.nepaliteacher.alexanepaliteacher.handlers;
 
+import static com.amazon.ask.request.Predicates.intentName;
+
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.model.Response;
@@ -9,11 +11,17 @@ public class FallBackIntentHandler implements RequestHandler {
 
   @Override
   public boolean canHandle(HandlerInput handlerInput) {
-    return false;
+
+    return handlerInput.matches(intentName("AMAZON.FallbackIntent"));
   }
 
   @Override
   public Optional<Response> handle(HandlerInput handlerInput) {
-    return Optional.empty();
+    String speechText = "Sorry, I don't know that. Would you like to learn from Nepali Teacher?";
+    return handlerInput.getResponseBuilder()
+        .withSpeech(speechText)
+        .withSimpleCard("Nepali Teacher", speechText)
+        .withReprompt(speechText)
+        .build();
   }
 }
