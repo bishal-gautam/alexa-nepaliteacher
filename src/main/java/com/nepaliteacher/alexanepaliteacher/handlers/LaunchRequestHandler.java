a4edea92fe89acbe4e316e79e24e88bf09d2f5da
bishal-gautam <bishal.gautam@careem.com>
2020-12-22 11:06:01 +0545

    processed launch requests

--- src/main/java/com/nepaliteacher/alexanepaliteacher/handlers/LaunchRequestHandler.java
+++ src/main/java/com/nepaliteacher/alexanepaliteacher/handlers/LaunchRequestHandler.java
@@ -1,7 +1,9 @@
 package com.nepaliteacher.alexanepaliteacher.handlers;
 
+import static com.amazon.ask.request.Predicates.requestType;
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.LaunchRequest;
 import com.amazon.ask.model.Response;
 import java.util.Optional;
 
@@ -9,11 +11,16 @@ public class LaunchRequestHandler implements RequestHandler {
 
   @Override
   public boolean canHandle(HandlerInput handlerInput) {
-    return false;
+    return handlerInput.matches(requestType(LaunchRequest.class));
   }
 
   @Override
   public Optional<Response> handle(HandlerInput handlerInput) {
-    return Optional.empty();
+    String speechText = "Welcome from Nepali Teacher. I will teach you Nepali. Would you like to learn Nepali alphabets?";
+    return handlerInput.getResponseBuilder()
+        .withSpeech(speechText)
+        .withSimpleCard("Nepali Teacher", speechText)
+        .withReprompt(speechText)
+        .build();
   }
 }
