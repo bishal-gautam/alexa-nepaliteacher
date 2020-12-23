a9953c0f26e2bcf5887d20ca25d3edaebd003382
bishal-gautam <bishal.gautam@careem.com>
2020-12-23 14:40:24 +0545

    processed alphabet requests

--- src/main/java/com/nepaliteacher/alexanepaliteacher/handlers/NepaliAlphabetIntentHandler.java
+++ src/main/java/com/nepaliteacher/alexanepaliteacher/handlers/NepaliAlphabetIntentHandler.java
@@ -14,6 +14,43 @@ public class NepaliAlphabetIntentHandler implements RequestHandler {
 
   @Override
   public Optional<Response> handle(HandlerInput handlerInput) {
-    return Optional.empty();
+    String speechText = "Letters of the Nepali alphabets are:"
+        + "ka "
+        + "kha "
+        + "ga "
+        + "gha "
+        + "ṅa "
+        + "ca "
+        + "cha "
+        + "ja "
+        + "jha "
+        + "ña "
+        + "ta "
+        + "tha "
+        + "da "
+        + "dha "
+        + "ṇa "
+        + "pa "
+        + "pha "
+        + "ba "
+        + "bha "
+        + "ma "
+        + "ya "
+        + "ra "
+        + "la "
+        + "va "
+        + "sa "
+        + "shha "
+        + "sha "
+        + "ha "
+        + "ksa "
+        + "tra "
+        + "jna ";
+
+    return handlerInput.getResponseBuilder()
+        .withSpeech(speechText)
+        .withSimpleCard("Nepali Teacher", speechText)
+        .withReprompt(speechText)
+        .build();
   }
 }
